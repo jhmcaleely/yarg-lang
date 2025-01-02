d1b23a641ce28cee5c4d21efe8ad084ad71b59dd
John McAleely <john@mcaleely.com>
2025-01-02 21:34:02 +0000

    Chapter 24: Calls and Functions

--- memory.c
+++ memory.c
@@ -21,6 +21,9 @@ static void freeObject(Obj* object) {
             freeChunk(&function->chunk);
             FREE(ObjFunction, object);
         }
+        case OBJ_NATIVE:
+            FREE(ObjNative, object);
+            break;
         case OBJ_STRING: {
             ObjString* string = (ObjString*)object;
             FREE_ARRAY(char, string->chars, string->length + 1);
