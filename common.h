92908c2ab9ede9b1f29d90036d2e589a705d4a3f
John McAleely <john@mcaleely.com>
2025-01-08 15:48:07 +0000

    Add build support for pico and pico2

--- common.h
+++ common.h
@@ -12,6 +12,14 @@
 //#define DEBUG_STRESS_GC
 //#define DEBUG_LOG_GC
 
+// Use a main() that only hosts a repl over stdin/stdout
+// Useful on microcontroller boards that have no filesystem, but 
+// have a serial terminal (somehow)
+//#define HOST_REPL
+
+// Set if we are building in the PICO SDK.
+//#define LOX_PICO_SDK
+
 #define UINT8_COUNT (UINT8_MAX + 1)
 
 #endif
