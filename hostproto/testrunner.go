a1ea42a563afea4ce6f764d64657bc6572529bd6
John McAleely <john@mcaleely.com>
2025-03-12 14:11:28 +0000

    some sketchy test running

--- hostproto/testrunner.go
+++ hostproto/testrunner.go
@@ -13,6 +13,14 @@ import (
 	"regexp"
 )
 
+type Test struct {
+	Expectations   int
+	Name           string
+	FileName       string
+	ExpectedOutput []string
+	ExpectedError  []string
+}
+
 func cmdRunTests(test string) {
 
 	test = filepath.Clean(test)
@@ -53,25 +61,35 @@ func cmdRunTests(test string) {
 	fmt.Printf("Total tests: %v, passed: %v\n", grandtotal, grandpass)
 }
 
-func runTestFile(test string) (total, pass int) {
-	test_name := filepath.Base(test)
-	extension := filepath.Ext(test)
+func testFriendlyName(testfile string) string {
+	test_name := filepath.Base(testfile)
+	extension := filepath.Ext(testfile)
 
-	parent_name := filepath.Base(filepath.Dir(test))
+	return test_name[:len(test_name)-len(extension)]
+}
 
-	test_friendly_name := test_name[:len(test_name)-len(extension)]
+func runTestFile(testfile string) (total, pass int) {
 
-	expectedResults := expectedResults(test)
-	total = len(expectedResults)
+	var test Test
+	test.FileName = testfile
+	test.Name = testFriendlyName(test.FileName)
 
-	output := parseTest(test)
+	test.parseTestSource()
+	total = test.Expectations
 
-	if reflect.DeepEqual(expectedResults[0:len(output)], output) {
+	output, error, _ := runTest(test.FileName)
+
+	if reflect.DeepEqual(test.ExpectedOutput[0:len(output)], output) {
 		pass = len(output)
 	}
 
+	if len(test.ExpectedError) > 0 &&
+		reflect.DeepEqual(test.ExpectedError, error) {
+		pass += len(error)
+	}
+
 	if total == 0 || pass != total {
-		fmt.Printf("test suite: %v:%v\n", parent_name, test_friendly_name)
+		fmt.Printf("test: %v\n", test.Name)
 		fmt.Printf("tests supplied: %v\n", total)
 		fmt.Printf("tests passed: %v\n", pass)
 	}
@@ -79,38 +97,67 @@ func runTestFile(test string) (total, pass int) {
 	return total, pass
 }
 
-func expectedResults(name string) []string {
-	tests, err := os.ReadFile(name)
+func (test *Test) parseTestSource() {
+	file, err := os.Open(test.FileName)
 	if err != nil {
-		log.Fatal("could not read test source")
+		log.Fatal("could not open test")
 	}
+	defer file.Close()
 
-	r := regexp.MustCompile("// expect: ?(.*)")
-
-	expectedResults := make([]string, 0, 4)
-
-	match := r.FindAllSubmatch(tests, -1)
-	for i := range match {
-		expectedResults = append(expectedResults, string(match[i][1]))
-	}
-	return expectedResults
-}
-
-func parseTest(name string) []string {
-	stdout := runtest(name)
-	scanner := bufio.NewScanner(&stdout)
-	results := make([]string, 0)
+	scanner := bufio.NewScanner(file)
+	var lineNo int
 	for scanner.Scan() {
-		results = append(results, scanner.Text())
+		lineNo++
+		test.parseLine(lineNo, scanner.Text())
 	}
-	return results
 }
 
-func runtest(name string) bytes.Buffer {
+func (test *Test) parseLine(lineNo int, line string) {
+	r := regexp.MustCompile(`// expect: ?(.*)`)
+	match := r.FindStringSubmatch(line)
+	if match != nil {
+		test.ExpectedOutput = append(test.ExpectedOutput, match[1])
+		test.Expectations++
+	}
+
+	r = regexp.MustCompile(`// (Error.*)`)
+	match = r.FindStringSubmatch(line)
+	if match != nil {
+		expected := fmt.Sprintf("[line %v] %v", lineNo, match[1])
+		test.ExpectedError = append(test.ExpectedError, expected)
+		test.Expectations++
+	}
+
+	r = regexp.MustCompile(`// expect runtime error: (.+)`)
+	match = r.FindStringSubmatch(line)
+	if match != nil {
+		test.ExpectedError = append(test.ExpectedError, match[1])
+		test.Expectations++
+		test.ExpectedError = append(test.ExpectedError, fmt.Sprintf("[line %v] in script", lineNo))
+		test.Expectations++
+	}
+
+}
+
+func parseOutput(stream bytes.Buffer) (lines []string) {
+	scanner := bufio.NewScanner(&stream)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
+}
+
+func runTest(name string) ([]string, []string, int) {
+	stdout, stderr, exit := runtestexecutable(name)
+	results := parseOutput(stdout)
+	errors := parseOutput(stderr)
+	return results, errors, exit
+}
+
+func runtestexecutable(name string) (cstdout, cstderr bytes.Buffer, exitcode int) {
 	interpreter := exec.Command("/Users/jhm/Developer/proto-lang/bin/clox", name)
-	var stdout, stderr bytes.Buffer
-	interpreter.Stdout = &stdout
-	interpreter.Stderr = &stderr
+	interpreter.Stdout = &cstdout
+	interpreter.Stderr = &cstderr
 
 	err := interpreter.Run()
 	if err != nil {
@@ -119,19 +166,20 @@ func runtest(name string) bytes.Buffer {
 			fmt.Println("failed executing:", err)
 		case *exec.ExitError:
 			if e.ExitCode() == 70 {
-				return stdout
+				return cstdout, cstderr, e.ExitCode()
 			} else if e.ExitCode() == 65 {
-				return stdout
+				return cstdout, cstderr, e.ExitCode()
 			} else {
-				fmt.Print(stdout.String())
-				fmt.Print(stderr.String())
+				fmt.Print(cstdout.String())
+				fmt.Print(cstderr.String())
 				fmt.Println("command exit rc =", e.ExitCode())
+				return bytes.Buffer{}, bytes.Buffer{}, e.ExitCode()
 			}
 		default:
 			panic(err)
 		}
 	} else {
-		return stdout
+		return cstdout, bytes.Buffer{}, 0
 	}
-	return bytes.Buffer{}
+	return bytes.Buffer{}, bytes.Buffer{}, 0
 }
