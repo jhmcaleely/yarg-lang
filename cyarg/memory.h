ed57f7808d4d0cf66172a3fb93baa98825834aaf
John McAleely <john@mcaleely.com>
2025-05-15 12:24:28 +0100

    Increase temp roots to pass more tests

--- cyarg/memory.h
+++ cyarg/memory.h
@@ -4,7 +4,7 @@
 #include "common.h"
 #include "object.h"
 
-#define TEMP_ROOTS_MAX 8
+#define TEMP_ROOTS_MAX 16
 
 #define ALLOCATE(type, count) \
     (type*)reallocate(NULL, 0, sizeof(type) * (count))
