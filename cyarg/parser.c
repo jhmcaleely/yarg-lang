ed2aa689aa6401e48ce2ff89462e628b54e4889f
John McAleely <john@mcaleely.com>
2025-05-23 17:16:45 +0100

    Precedence belongs in praser.c

--- cyarg/parser.c
+++ cyarg/parser.c
@@ -9,6 +9,21 @@
 #include "scanner.h"
 #include <stdbool.h>
 
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT, // =
+    PREC_OR,         // or
+    PREC_AND,        // and
+    PREC_EQUALITY,   // == !=
+    PREC_COMPARISON, // < > <= >=
+    PREC_TERM,       // + - | ^ & %
+    PREC_FACTOR,     // * / << >>
+    PREC_UNARY,      // ! -
+    PREC_CALL,       // . ()
+    PREC_DEREF,      // []
+    PREC_PRIMARY
+} Precedence;
+
 typedef struct {
     Token current;
     Token previous;
