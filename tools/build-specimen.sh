46c2d7a26b2c8ea60918cc29741d0d3b2a87914a
John McAleely <john@mcaleely.com>
2025-09-26 13:59:25 +0100

    add serial-echo

--- tools/build-specimen.sh
+++ tools/build-specimen.sh
@@ -26,6 +26,7 @@ fi
 ../../bin/hostyarg addfile -fs ../../build/specimen-fs.uf2 -add timed-flash.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen-fs.uf2 -add ws2812.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen-fs.uf2 -add button.ya
+../../bin/hostyarg addfile -fs ../../build/specimen-fs.uf2 -add serial-echo.ya
 ../../bin/hostyarg addfile -fs ../../build/specimen-fs.uf2 -add main.ya
 
 popd > /dev/null
