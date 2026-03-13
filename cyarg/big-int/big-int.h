a918ba3826f6ab8b873a655e2e298fe17e99fa2f
John McAleely <john@mcaleely.com>
2026-03-13 12:07:00 +0000

    Prefer an integer literal for Array Initialisation cardinality
      - includes review comments from @dlm-yarg 14/3/26

--- cyarg/big-int/big-int.h
+++ cyarg/big-int/big-int.h
@@ -15,8 +15,12 @@
 #define INT_DIGITS_FOR_INT32 2
 #define INT_DIGITS_FOR_INT64 4
 #define INT_DIGITS_FOR_ADDRESS 4 // target address size could be 32 or 64
-#define INT_DIGITS_FOR_S(STRLEN) ((1651124 + (int)(STRLEN) * 342808) / 1651125) // multiply by 1/log10(65536) - fails at 1224 decimal digits (255 (16-bit) digits)
 #define INT_STRLEN_FOR_INT254 1226 // ceil(log10(pow(65536, 254))) + 1 (for null) + 1 for '-'
+
+// For the numerical parts of a decimal string representation (ie, not including a sign symbol)
+// multiply by 1/log10(65536) - fails at 1224 decimal digits (255 (16-bit) digits)
+#define INT_DIGITS_FOR_S(STRLEN) ((1651124 + (int)(STRLEN) * 342808) / 1651125)
+
 
 typedef struct
 {
