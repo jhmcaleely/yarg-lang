113fa406e16a929cde45589f6deb06fdb699eb02
John McAleely <john@mcaleely.com>
2026-03-13 14:26:33 +0000

    Some startup changes and a simple test.

--- cyarg/main.c
+++ cyarg/main.c
@@ -34,40 +34,48 @@ static void repl() {
 }
 
 #ifdef CYARG_FEATURE_HOSTED_REPL
-static void runFile(const char* path) {
+static int runFile(const char* path) {
     char* source = readFile(path);
     if (source) {
         InterpretResult result = interpret(source);
         free(source);
 
-        if (result == INTERPRET_COMPILE_ERROR) exit(EX_DATAERR);
-        if (result == INTERPRET_RUNTIME_ERROR) exit(EX_SOFTWARE);
+        if (result == INTERPRET_COMPILE_ERROR) {
+            return EX_DATAERR;
+        } else if (result == INTERPRET_RUNTIME_ERROR) {
+            return EX_SOFTWARE;
+        } else {
+            return EX_OK;
+        }
     } else {
-        exit(EX_NOINPUT);
+        return EX_NOINPUT;
     }
 }
 
-static void compileFile(const char* path) {
+static int compileFile(const char* path) {
     char* source = readFile(path);
     if (!source) {
-        exit(EX_NOINPUT);
+        return EX_NOINPUT;
     }
 
     ObjFunction* result = compile(source);
+    free(source);
     if (!result) {
-        exit(EX_DATAERR);
+        return EX_DATAERR;
     }
+    return EX_OK;
 }
 
-static void disassembleFile(const char* path) {
+static int disassembleFile(const char* path) {
     char* source = readFile(path);
     if (!source) {
-        exit(EX_NOINPUT);
+        return EX_NOINPUT;
     }
 
     ObjFunction* result = compile(source);
+    free(source);
     if (!result) {
-        exit(EX_DATAERR);
+        return EX_DATAERR;
     }
 
     disassembleChunk(&result->chunk, path);
@@ -77,6 +85,7 @@ static void disassembleFile(const char* path) {
             disassembleChunk(&fun->chunk, fun->name->chars);
         }
     }
+    return EX_OK;
 }
 #endif
 
@@ -96,25 +105,37 @@ int main() {
     return 0;
 }
 #elif defined(CYARG_FEATURE_HOSTED_REPL)
+void usageMessage(FILE* destination) {
+    FPRINTMSG(destination, "Usage:\n");
+    FPRINTMSG(destination, "\tcyarg <path>\n");
+    FPRINTMSG(destination, "\tcyarg --help\n");
+    FPRINTMSG(destination, "\tcyarg --compile <path>\n");
+    FPRINTMSG(destination, "\tcyarg --disassemble <path>\n");
+}
+
 int main(int argc, const char* argv[]) {
     plaform_hal_init();
     initVM();
 
+    int returnCode = EX_OK;
+
     if (argc == 1) {
         repl();
+    } else if (argc == 2 && strcmp(argv[1], "--help") == 0) {
+        usageMessage(stdout);
     } else if (argc == 2) {
-        runFile(argv[1]);
+        returnCode = runFile(argv[1]);
     } else if (argc == 3 && strcmp(argv[1], "--compile") == 0) {
-        compileFile(argv[2]);
+        returnCode = compileFile(argv[2]);
     } else if (argc == 3 && strcmp(argv[1], "--disassemble") == 0) {
-        disassembleFile(argv[2]);
+        returnCode = disassembleFile(argv[2]);
     }
     else {
-        FPRINTMSG(stderr, "Usage: cyarg [path]\n");
-        exit(EX_USAGE);
+        usageMessage(stderr);
+        returnCode = EX_USAGE;
     }
 
     freeVM();
-    return 0;
+    return returnCode;
 }
 #endif
---
+++ test/cyarg-run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+INTERPRETER="./bin/cyarg"
+
+$INTERPRETER --help
