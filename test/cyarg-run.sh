4902d1b1f6472176ab3bfbddb720e23e497a5ea1
John McAleely <john@mcaleely.com>
2026-03-19 10:45:43 +0000

    test cyarg as a separate tool to hostyarg/cmd/yarg

--- test/cyarg-run.sh
+++ test/cyarg-run.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-INTERPRETER="./bin/cyarg"
-CYARG_ERROR=0
 
-$INTERPRETER --help || CYARG_ERROR=$?
-$INTERPRETER --lib yarg/specimen test/cyarg/simple.ya || CYARG_ERROR=$?
-$INTERPRETER --lib yarg/specimen test/cyarg/compile-error.ya
-ERROR=$?
-if [ $ERROR -ne 65 ]; then
-    echo "Expected compile error, got exit code $ERROR"
-    CYARG_ERROR=1
-fi
-$INTERPRETER --compile test/cyarg/compile-error.ya
-ERROR=$?
-if [ $ERROR -ne 65 ]; then
-    echo "Expected compile error, got exit code $ERROR"
-    CYARG_ERROR=1
-fi
-$INTERPRETER --disassemble test/cyarg/simple.ya || CYARG_ERROR=$?
-exit $CYARG_ERROR
+./test/cyarg/cyarg.sh 2>&1 | diff - test/cyarg/cyarg.expected
