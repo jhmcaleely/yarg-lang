e46b7c5b4543a189707afd7241b8d14a5233eeca
dlm <dlm.yarg@outlook.com>
2026-04-09 18:14:50 +1200

    cp

--- cyarg/pack.c
+++ cyarg/pack.c
@@ -17,10 +17,7 @@ typedef struct {
 typedef struct {
     uint32_t n_;
     uint32_t extent_;
-    struct {
-        uint32_t offset_;
-        uint32_t line_;
-    } *i_;
+    uint32_t *i_;
 } LinesFile;
 
 typedef struct {
@@ -33,6 +30,12 @@ typedef struct {
     uint32_t n_;
     uint32_t extent_;
     uint32_t *i_;
+} OffsetsFile;
+
+typedef struct {
+    uint32_t n_;
+    uint32_t extent_;
+    char *i_;
 } StringsFile;
 
 typedef struct {
@@ -44,89 +47,128 @@ typedef struct {
 typedef struct {
     uint32_t n_;
     uint32_t extent_;
-    Int **i_;
+    uint32_t *i_;
 } IntsFile;
 
 typedef struct {
+    OffsetsFile codeOffsetsFile_;
     CodeFile codeFile_;
     LinesFile linesFile_;
-    ChunksFile chunksFile_;
+    OffsetsFile stringOffsetsFile_;
     StringsFile stringsFile_;
     DoublesFile doublesFile_;
+    OffsetsFile intOffsetsFile_;
     IntsFile intsFile_;
 } Files;
 
 static void fileExtend(void *, int, size_t);
-static void fileExtendIfNeeded(void *, size_t);
-static void packAnotherChunk(Chunk *, bool, Files *);
+static void packConstants(Chunk *, bool, Files *);
+static void packCode(Chunk *, bool, Files *);
 static int packInstruction(Chunk *, int, Files *);
 
 char *packChunk(Chunk *chunk, bool includeLines, int *len)
 {
     Files f = {0};
-    fileExtend(&f.codeFile_, 1024, sizeof *f.codeFile_.i_);
+    fileExtend(&f.codeOffsetsFile_, 4, sizeof *f.codeOffsetsFile_.i_);
+    fileExtend(&f.codeFile_, 512, sizeof *f.codeFile_.i_);
     fileExtend(&f.linesFile_, 128, sizeof *f.linesFile_.i_);
-    fileExtend(&f.chunksFile_, 8, sizeof *f.chunksFile_.i_);
-    fileExtend(&f.stringsFile_, 32, sizeof *f.stringsFile_.i_);
-    fileExtend(&f.doublesFile_, 8, sizeof *f.doublesFile_.i_);
-    fileExtend(&f.intsFile_, 16, sizeof *f.intsFile_.i_);
+    fileExtend(&f.stringOffsetsFile_, 16, sizeof *f.stringOffsetsFile_.i_);
+    fileExtend(&f.stringsFile_, 128, sizeof *f.stringsFile_.i_);
+    fileExtend(&f.doublesFile_, 4, sizeof *f.doublesFile_.i_);
+    fileExtend(&f.intOffsetsFile_, 8, sizeof *f.intOffsetsFile_.i_);
+    fileExtend(&f.intsFile_, 256, sizeof *f.intsFile_.i_);
 
-    packAnotherChunk(chunk, includeLines, &f);
+    packConstants(chunk, includeLines, &f);
+    fileExtend(&f.stringOffsetsFile_, 1, sizeof *f.stringOffsetsFile_.i_);
+    f.stringOffsetsFile_.i_[f.stringOffsetsFile_.n_++] = f.stringsFile_.n_;
+    // todo remove duplicates and sort
 
-    // magic
-    // version
-    // C code bytes
-    // L lines -- optionally zero
-    // M chunks
-    // N string bytes
-    // O doubles
-    // P int offsets
-    // double *O1(8)
-    // int offsets *P1(3)
-    // ints *
-    // strings *N3
-    // code offsets *M3(3)
-    // pair<offset, line> *L
-    // code *C
+    packCode(chunk, includeLines, &f);
 
-    char *binary = realloc(0, 4096);
+    // 0    magic(6) - 79 0a 72 67 00 42
+    // 6    version(3) - 26, 52, 01 year, week, issue
+    // 9  M code offsets(1)
+    // 10 C code 4-bytes(2)
+    // 12 L lines(2) -- optionally zero
+    // 14 T string 4-bytes(2)
+    // 16 N strings(2)
+    // 18 O doubles(2)
+    // 20 P int offsets(2)
+    // 22 Q int words(2)
+    // 24   double *O1(8)
+    // x8   ints *Q
+    // x4   int offsets(2) *P
+    // x2   offset(3) *L -- optional
+    // x1   string offsets(3) *(N+1) -- extra offset to determine length of last string
+    // x1   strings *(T*4)
+    // x1   code offsets(3) *M
+    // x1   code *(C*4)
+
+    int stringLen = (f.stringsFile_.n_ + 3) / 4;
+    int codeLen = (f.codeFile_.n_ + 3) / 4;
+    int totalLen = 24 +
+                    8 * f.doublesFile_.n_ +
+                    4 * f.intsFile_.n_ + 2 * f.intOffsetsFile_.n_ +
+                    3 * f.linesFile_.n_ +
+                    3 * (f.stringOffsetsFile_.n_ + 1) + 4 * stringLen +
+                    3 * f.codeOffsetsFile_.n_ + 4 * codeLen;
+    char *binary = realloc(0, totalLen);
     len = 0;
+
+    // generate
+
+    free(f.intsFile_.i_);
+    free(f.intOffsetsFile_.i_);
+    free(f.doublesFile_.i_);
+    free(f.stringsFile_.i_);
+    free(f.stringOffsetsFile_.i_);
+    free(f.linesFile_.i_);
+    free(f.codeFile_.i_);
+    free(f.codeOffsetsFile_.i_);
 
     return binary;
 }
 
-static void packAnotherChunk(Chunk* chunk, bool includeLines, Files *f) {
-    struct {
-        uint32_t n_;
-        uint32_t extent_;
-        struct {
-            uint8_t constant_;
-            uint32_t xip_;
-        } *i_;
-    } relocationTable = {0};
+/////  todo  file and sort strings first
+//////            mark constants as used when parsing byte code
+//////            methods + properties of returned struct or class, global var names, 1st level function names are exported, op_const are not
+///////            returned includes copied to var parameters
+//////            remove non-global consts
 
-    fileExtend(&relocationTable, 64, sizeof *relocationTable.i_);
-
+void packConstants(Chunk *chunk, bool includeLines, Files *f) {
     for (int i = 0; i < chunk->constants.count; i++) {
         Value *v = &chunk->constants.values[i];
         if (IS_DOUBLE(*v)) {
-            fileExtendIfNeeded(&relocationTable, sizeof *relocationTable.i_);
-            // add to file
-            // add to relocation table
+            fileExtend(&f->doublesFile_, 4, sizeof *f->doublesFile_.i_);
+            f->doublesFile_.i_[f->doublesFile_.n_++] = AS_DOUBLE(*v);
         } else if (IS_OBJ(*v)) {
             switch (AS_OBJ(*v)->type) {
             case OBJ_FUNCTION:
                 break;
-            case OBJ_INT:
-                fileExtendIfNeeded(&relocationTable, sizeof *relocationTable.i_);
-                // add to file
-                // add to relocation table
+            case OBJ_INT: {
+                fileExtend(&f->intOffsetsFile_, 8, sizeof *f->intOffsetsFile_.i_);
+                f->intOffsetsFile_.i_[f->intOffsetsFile_.n_++] = f->intsFile_.n_;
+                Int *from = AS_INT(*v);
+                int len = (int)((char *) from->w_ - (char *) from);
+                assert(len % 4 == 0);
+                len += sizeof (uint16_t) * from->d_;
+                len += len % 4;
+                fileExtend(&f->intsFile_, len / 4, sizeof *f->intsFile_.i_);
+                memcpy(&f->intsFile_.i_[f->intsFile_.n_], from, len);
+                f->intsFile_.n_ += len / 4;
                 break;
-            case OBJ_STRING:
-                fileExtendIfNeeded(&relocationTable, sizeof *relocationTable.i_);
-                // add to file
-                // add to relocation table
+            }
+            case OBJ_STRING: {
+                fileExtend(&f->stringOffsetsFile_, 16, sizeof *f->stringOffsetsFile_.i_);
+                f->stringOffsetsFile_.i_[f->stringOffsetsFile_.n_++] = f->stringsFile_.n_;
+                ObjString *from = AS_STRING(*v);
+                int len = from->length;
+                fileExtend(&f->stringsFile_, len + 1, sizeof *f->stringsFile_.i_);
+                assert(len < f->stringsFile_.extent_ - f->stringsFile_.n_);
+                memcpy(&f->stringsFile_.i_[f->stringsFile_.n_], from->chars, len);
+                f->stringsFile_.n_ += len;
                 break;
+            }
             default:
                 assert(!"Unexpected constant obj");
                 break;
@@ -135,6 +177,25 @@ static void packAnotherChunk(Chunk* chunk, bool includeLines, Files *f) {
             assert(!"Unexpected constant value");
         }
     }
+
+    for (int i = 0; i < chunk->constants.count; i++) {
+        Value *v = &chunk->constants.values[i];
+        if (IS_OBJ(*v)) {
+            switch (AS_OBJ(*v)->type) {
+            case OBJ_FUNCTION:
+                packConstants(&AS_FUNCTION(*v)->chunk, includeLines, f);
+            default:
+                break;
+            }
+        }
+    }
+}
+
+static void packCode(Chunk *chunk, bool includeLines, Files *f) {
+    fileExtend(&f->codeOffsetsFile_, 4, sizeof *f->codeOffsetsFile_.i_);
+    f->codeOffsetsFile_.i_[f->codeOffsetsFile_.n_++] = f->codeFile_.n_;
+
+    fileExtend(&f->codeFile_, 512, sizeof *f->codeFile_.i_);
 
     for (int offset = 0; offset < chunk->count;) {
         offset = packInstruction(chunk, offset, f);
@@ -145,15 +206,14 @@ static void packAnotherChunk(Chunk* chunk, bool includeLines, Files *f) {
         if (IS_OBJ(*v)) {
             switch (AS_OBJ(*v)->type) {
             case OBJ_FUNCTION:
-                packAnotherChunk(&AS_FUNCTION(*v)->chunk, includeLines, f);
+                packCode(&AS_FUNCTION(*v)->chunk, includeLines, f);
             default:
                 break;
             }
         }
     }
-
-    free(relocationTable.i_);
 }
+
 static char const *valueType(Value *v) {
     switch (v->type) {
     case VAL_BOOL: return "bool";
@@ -475,17 +535,8 @@ void fileExtend(void *f, int n, size_t i)
 {
     CodeFile *file = (CodeFile *) f;
 
-    file->extent_ += n;
-    file->i_ = realloc(file->i_, file->extent_ * i);
-}
-
-void fileExtendIfNeeded(void *f, size_t i)
-{
-    CodeFile *file = (CodeFile *) f;
-
-    if (file->n_ == file->extent_)
-    {
-        file->extent_ += file->extent_ / 2;
+    if (n > file->extent_ - file->n_) {
+        file->extent_ += n * 2;
         file->i_ = realloc(file->i_, file->extent_ * i);
     }
 }
