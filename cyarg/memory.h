888f89822be0dc0d628b42bdcb30d4c4580118b3
John McAleely <john@mcaleely.com>
2026-09-15 16:46:32 +0100

    compile yarg.ya into cyarg as yarg.yb

--- cyarg/memory.h
+++ cyarg/memory.h
@@ -8,7 +8,7 @@ typedef struct O1HeapInstance O1HeapInstance;
 
 void init_heap_instance(O1HeapInstance** instance);
 
-#define TEMP_ROOTS_MAX 8
+#define TEMP_ROOTS_MAX 16
 #define FIRST_GC_AT 50 * 1024
 #define ALWAYS_GC_ABOVE 100 * 1024
 
